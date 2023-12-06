6d32a257245f29167731c2ea9951a5d9c5a2e21b
htchsdg <2016306815@qq.com>
2023-12-06 22:52:37 +0800

    add employee

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +17,10 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    /**
+     * 新增员工
+     * @param employee
+     */
+
+    void insert(Employee employee);
 }
